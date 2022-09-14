b197aff9416d77d1024b9b1983c162dc16254b5d
Sweta Kumari <74856144+Sweta8587@users.noreply.github.com>
2022-09-14 23:27:22 +0530

    Update tut01.py
    changing lib

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,7 +1,2 @@
 import csv
-with open('octant_input.csv', 'r') as file:
-    file = csv.reader(file)
-    with open('new_octant.csv', 'w')as csv_file:
-        csv_file = csv.writer(csv_file)
-        for line in file:
-            csv_file.writerow(line)
+from math import ceil, floor
