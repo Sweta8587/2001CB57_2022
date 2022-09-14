39d58d2884bcacbf2e31f6fb5954232c9601f68e
Sweta Kumari <74856144+Sweta8587@users.noreply.github.com>
2022-09-14 23:37:40 +0530

    Update tut01.py
    program to determine octant

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,2 +1,20 @@
 import csv
 from math import ceil, floor
+
+def get_octant(x, y, z):
+    if(x>=0 and y>=0 and z>=0):
+        return("octant is 1")
+    elif(x>=0 and y>=0 and z<0):
+        return("octant is -1")
+    elif(x<0 and y>=0 and z>=0):
+        return("octant is 2")
+    elif(x<0 and y>=0 and z<0):
+        return("octant is -2")
+    elif(x<0 and y<0 and z>=0):
+        return("octant is 3")
+    elif(x<0 and y<0 and z<0):
+        return("octant is -3")
+    elif(x>=0 and y<0 and z>=0):
+        return("octant is 4")
+    else:
+        return("octant is -4")
