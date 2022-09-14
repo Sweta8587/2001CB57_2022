a97002c568be5e7ec98aa8d3197206dacb712041
Sweta Kumari <74856144+Sweta8587@users.noreply.github.com>
2022-09-14 23:45:29 +0530

    Update tut01.py
    step 2

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -18,3 +18,54 @@ def get_octant(x, y, z):
         return("octant is 4")
     else:
         return("octant is -4")
+
+def octact_identification(mod=5000):
+    
+    # values of original
+    T, U, V, W=[], [], [], []
+
+    # processed values 
+    U_, V_, W_=[], [], []
+
+    # sum values 
+    Uavg, Vavg, Wavg=0, 0, 0
+
+    # count of values
+    n=0  
+
+    try:
+        with open('octant_input.csv', 'r') as inputfile:
+            freader=csv.reader(inputfile)
+
+            # now skipping the heading row
+            next(freader)
+
+            for line in freader:
+                Uavg+=float(line[1])
+                U.append(float(line[1]))
+
+                Vavg+=float(line[2])
+                V.append(float(line[2]))
+
+                Wavg+=float(line[3])
+                W.append(float(line[3]))
+
+                T.append(line[0])
+                n+=1
+
+            #  average values calculation
+            Uavg=Uavg/n
+            Vavg=Vavg/n
+            Wavg=Wavg/n
+
+            for i in range(0,n):
+                x=U[i]-Uavg
+                U_.append(x)
+
+                y=V[i]-Vavg
+                V_.append(y)
+                
+                z=W[i]-Wavg
+                W_.append(z)
+
+            nr=ceil(n/mod)        
