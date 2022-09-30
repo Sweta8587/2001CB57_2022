5b82fde1c1db20b9bd530d35bd191b4982729590
Sweta Kumari <74856144+Sweta8587@users.noreply.github.com>
2022-09-30 22:16:58 +0530

    Update tut02.py
    tutorial 2 start

--- tut02/tut02.py
+++ tut02/tut02.py
@@ -1,13 +1,27 @@
-def octant_transition_count(mod=5000):
-###Code
+# def octant_transition_count(mod=5000):
+# ###Code
 
-from platform import python_version
-ver = python_version()
+# from platform import python_version
+# ver = python_version()
 
-if ver == "3.8.10":
-    print("Correct Version Installed")
-else:
-    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+# if ver == "3.8.10":
+#     print("Correct Version Installed")
+# else:
+#     print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
 
+# mod=5000
+# octant_transition_count(mod)
+
+
+###### defining mod value
 mod=5000
-octant_transition_count(mod)
+
+##### importing openpyxl and nan and loading workbook
+try:
+    from cmath import nan
+    import openpyxl
+
+    wb = openpyxl.load_workbook(r'C:\Users\DELL\OneDrive\Desktop\temp_octant\input_octant_transition_identify - Copy.xlsx')
+except:
+    print("there is error in loading workbook check your file directory and import openpyxl")
+    exit()
