ab57fa9e42b8671eece19831e285ffe42ac0fffb
Sweta Kumari <74856144+Sweta8587@users.noreply.github.com>
2022-10-12 00:07:07 +0530

    Update tut03.py
    step 3

--- tut03/tut03.py
+++ tut03/tut03.py
@@ -58,3 +58,51 @@ def octant_longest_subsequence_count():
         u_s=int(u_s*1000000000); u_s/=1000000000
         v_s=int(v_s*1000000000); v_s/=1000000000
         w_s=int(w_s*1000000000); w_s/=1000000000
+
+        for i in range(2, nr+1):
+            x=input_sh.cell(i,2).value - u_s; x=int(x*1000000000); x/=1000000000; ud.append(x)
+            y=input_sh.cell(i,3).value - v_s; y=int(y*1000000000); y/=1000000000; vd.append👍
+            z=input_sh.cell(i,4).value - w_s; z=int(z*1000000000); z/=1000000000; wd.append(z)
+            o=get_octant(x,y,z); ov.append(o)
+
+        # output xlsx file
+        wb_output=Workbook()
+        output_sh=wb_output.active
+
+        # printing all initial values, average values, preprocessed values
+        output_sh.cell(1,1, value='Time')
+        output_sh.cell(1,2, value='U')
+        output_sh.cell(1,3, value='V')
+        output_sh.cell(1,4, value='W')
+        for i in range(2, nr+1):
+            output_sh.cell(i,1, value=input_sh.cell(i,1).value)
+            output_sh.cell(i,2, value=input_sh.cell(i,2).value)
+            output_sh.cell(i,3, value=input_sh.cell(i,3).value)
+            output_sh.cell(i,4, value=input_sh.cell(i,4).value)
+
+
+        output_sh.cell(1,5, value='U Avg'); output_sh.cell(2,5, value=u_s)
+        output_sh.cell(1,6, value='V Avg'); output_sh.cell(2,6, value=v_s)
+        output_sh.cell(1,7, value='W Avg'); output_sh.cell(2,7, value=w_s)
+
+        output_sh.cell(1,8, value="U'=U - U avg")
+        output_sh.cell(1,9, value="V'=V - V avg")
+        output_sh.cell(1,10, value="W'=W - W avg")
+        output_sh.cell(1,11, value="Octact")
+        j=0
+        for i in range(2,nr+1):
+            output_sh.cell(i,8, value=ud[j])
+            output_sh.cell(i,9, value=vd[j])
+            output_sh.cell(i,10, value=wd[j])
+            output_sh.cell(i,11, value=ov[j])
+            j+=1
+        
+        # longest subsequence and count
+        lsc={1:[0,0], -1:[0,0], 2:[0,0], -2:[0,0], 3:[0,0], -3:[0,0], 4:[0,0], -4:[0,0]}
+
+        # temp octant value list, last element as terminator
+        tov=ov.copy(); tov.append(0)
+
+        # cuurent octant, current octant length
+        co=ov[0]; col=1
+        lsc[co][0]=1; lsc[co][1]=1
