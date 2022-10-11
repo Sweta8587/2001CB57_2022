b7ba5659e473138995508bd7ff4b55124deacb1f
Sweta Kumari <74856144+Sweta8587@users.noreply.github.com>
2022-10-12 00:06:09 +0530

    Update tut03.py
    step 2

--- tut03/tut03.py
+++ tut03/tut03.py
@@ -22,3 +22,39 @@ def get_octant(x, y, z):
         return(4)
     else:
         return(-4)
+
+
+def octant_longest_subsequence_count():
+    # processed values (to be used in program)
+    ud, vd, wd, ov=[], [], [], []
+
+    # sum values
+    u_s, v_s, w_s=0, 0, 0
+
+    # count of values
+    n=0
+    
+    try:
+
+        # input xlsx file 
+        wb_input=openpyxl.load_workbook("input_octant_longest_subsequence.xlsx")
+        input_sh=wb_input.active
+
+        # getting number of rows
+        nr=input_sh.max_row
+
+        # n (no of values) = number of rows(nr) - header row(1)
+        n=nr-1
+
+        for i in range(2, nr+1):
+            u_s+=input_sh.cell(i,2).value
+            v_s+=input_sh.cell(i,3).value
+            w_s+=input_sh.cell(i,4).value
+
+        # calculating average values
+        u_s/=n; v_s/=n; w_s/=n
+
+        #reporting data upto 9 decimal places
+        u_s=int(u_s*1000000000); u_s/=1000000000
+        v_s=int(v_s*1000000000); v_s/=1000000000
+        w_s=int(w_s*1000000000); w_s/=1000000000
