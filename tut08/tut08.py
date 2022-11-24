c6fe5731a04484748d42d4f6cc7247ff76efe453
Sweta Kumari <74856144+Sweta8587@users.noreply.github.com>
2022-11-24 23:02:46 +0530

    Update tut08.py
    step 2

--- tut08/tut08.py
+++ tut08/tut08.py
@@ -62,3 +62,106 @@ lb=0
 fall=''
 
 content=[]
+
+
+for i in range(lx): #iterating through every ball 
+  ball_no=line[i][0]
+  players=line[i][1].split('to') 
+  batsname=players[1]            #finding batsamn and bowler names for every deliery
+  bowlername=players[0]
+
+  line[i][2]=line[i][2].lower()  #converting the verdict of the delivery into lower case
+
+  if line[i][2]==" wide":  #if the delivery is wide
+   wide=wide+1             #adding +1 to wide variable
+   score=score+1
+   mybowlers.loc[bowlername,'R']=mybowlers.loc[bowlername,'R']+1  #adding 1 run to the particular bowler's runs
+   mybowlers.loc[bowlername,'WD']=mybowlers.loc[bowlername,'WD']+1 # increaing count of wide by +1
+
+  elif line[i][2]==" four" or line[i][2]==' 4' or line[i][2]==' 4 runs': #if the verdict of the delivery if '4'
+   mybatsman.loc[batsname,'R']=mybatsman.loc[batsname,'R']+4  
+   mybatsman.loc[batsname,'4s']=mybatsman.loc[batsname,'4s']+1
+   mybatsman.loc[batsname,'B']=mybatsman.loc[batsname,'B']+1
+   mybowlers.loc[bowlername,'B']=mybowlers.loc[bowlername,'B']+1
+   mybowlers.loc[bowlername,'R']=mybowlers.loc[bowlername,'R']+4
+   score=score+4
+
+  elif line[i][2] == ' six' or line[i][2] == ' 6' or line[i][2] == ' 6 runs': #if the verdict of the ball is '6'
+   mybatsman.loc[batsname,'6s'] = mybatsman.loc[batsname,'6s'] + 1
+   mybatsman.loc[batsname,'R'] = mybatsman.loc[batsname,'R'] + 6
+   mybatsman.loc[batsname,'B'] = mybatsman.loc[batsname,'B'] + 1
+
+   mybowlers.loc[bowlername,'B'] = mybowlers.loc[bowlername,'B'] + 1
+   mybowlers.loc[bowlername,'R'] = mybowlers.loc[bowlername,'R'] + 6
+   score = score +6
+   
+  elif line[i][2] == ' 1 run' or line[i][2] == ' 1': #if the verdict of the ball is'1' run
+        mybatsman.loc[batsname,'R'] = mybatsman.loc[batsname,'R'] + 1   
+        mybatsman.loc[batsname,'B'] = mybatsman.loc[batsname,'B'] + 1
+        
+        mybowlers.loc[bowlername,'B'] = mybowlers.loc[bowlername,'B'] + 1
+        mybowlers.loc[bowlername,'R'] = mybowlers.loc[bowlername,'R'] + 1
+        score = score +1
+    
+    
+  elif line[i][2] == ' 2 runs' or line[i][2] == ' 2 run' or line[i][2] == ' 2': #it the verdict of the ball is '2' runs
+        mybatsman.loc[batsname,'R'] = mybatsman.loc[batsname,'R'] + 2 
+        mybatsman.loc[batsname,'B'] = mybatsman.loc[batsname,'B'] + 1
+        
+        mybowlers.loc[bowlername,'B'] = mybowlers.loc[bowlername,'B'] + 1
+        mybowlers.loc[bowlername,'R'] = mybowlers.loc[bowlername,'R'] + 2
+        score = score +2
+    
+   
+  elif line[i][2] == ' 3 runs' or line[i][2] == ' 3 run' or line[i][2] == ' 3': #it the verdict of the ball is '3' runs
+        mybatsman.loc[batsname,'R'] = mybatsman.loc[batsname,'R'] + 3
+        mybatsman.loc[batsname,'B'] = mybatsman.loc[batsname,'B'] + 1
+        
+        mybowlers.loc[bowlername,'B'] = mybowlers.loc[bowlername,'B'] + 1
+        mybowlers.loc[bowlername,'R'] = mybowlers.loc[bowlername,'R'] + 3
+        score = score +3
+  elif line[i][2] == ' no run' or line[i][2] == ' no': #if delivery is a dot and legal
+        mybatsman.loc[batsname,'B'] = mybatsman.loc[batsname,'B'] + 1 
+        mybowlers.loc[bowlername,'B'] = mybowlers.loc[bowlername,'B'] + 1
+    
+   
+  elif line[i][2] == ' leg byes' : #if delivery verdict is leg byes
+        mybatsman.loc[batsname,'B'] = mybatsman.loc[batsname,'B'] + 1        
+        mybowlers.loc[bowlername,'B'] = mybowlers.loc[bowlername,'B'] + 1
+                        
+        if line[i][3] == ' four' or line[i][3] == ' 4' or line[i][3] == ' 4 runs':#for 4 leg byes
+            lb = lb + 4
+            score = score +4
+
+        if line[i][3] == ' 1 run' or line[i][3] == ' 1': #for 1leg bye
+            lb = lb + 1
+            score = score +1
+
+  elif line[i][2] == ' byes': #if delivery verdict is bye
+        mybatsman.loc[batsname,'B'] = mybatsman.loc[batsname,'B'] + 1
+        mybowlers.loc[bowlername,'B'] = mybowlers.loc[bowlername,'B'] + 1
+                
+        if line[i][3] == ' 1 run' or line[i][3] == ' 1': #for 1bye
+            b = b + 1
+            score = score +1
+  else:   #if the verdict of the delivery is a wicket 
+        mybatsman.loc[batsname,'B'] = mybatsman.loc[batsname,'B'] + 1
+        mybowlers.loc[bowlername,'B'] = mybowlers.loc[bowlername,'B'] + 1
+        wickets = wickets +1   #increasing wicket count
+        add = str(score) + '-' + str(wickets) + '('+ batsname + ',' + ball_no + ')'#adding to the the fall of wickets string
+        fall=fall+add
+        fall=fall+" "
+        
+        content = line[i][2].split('!')  #spltting @ '!'
+        
+        if content[0] == ' out bowled': #if the mode of wicket is 'bowled'
+            mybatsman.loc[batsname,'status'] = 'b ' + bowlername 
+            mybowlers.loc[bowlername,'W'] = mybowlers.loc[bowlername,'W'] + 1
+        elif content[0]==' out lbw':   #if the mode of wicket is 'lbw'
+         mybatsman.loc[batsname,'status'] = 'lbw ' + bowlername 
+         mybowlers.loc[bowlername,'W'] = mybowlers.loc[bowlername,'W'] + 1
+        else: #if the mode of wicket is 'caught out'
+            content2 = content[0].split('by')
+            mybatsman.loc[batsname,'status'] = 'c ' + content2[1] + ' b ' + bowlername #updating status of the batsman
+            mybowlers.loc[bowlername,'W'] = mybowlers.loc[bowlername,'W'] + 1
+     
