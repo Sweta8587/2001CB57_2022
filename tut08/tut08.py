ea4b7905982346d8d20e6cf107857cb91529ed6b
Sweta Kumari <74856144+Sweta8587@users.noreply.github.com>
2022-11-24 23:01:24 +0530

    tut 8
    step

--- tut08/tut08.py
+++ tut08/tut08.py
@@ -1,30 +1,64 @@
+import pandas as pd
+import re 
+text= open("pak_inns1.txt","r").read() #reading 1st innings input file into 'text'
 
-from datetime import datetime
-start_time = datetime.now()
+send=[]
 
-#Help
-def scorecard():
-	pass
-
-
-###Code
-
-from platform import python_version
-ver = python_version()
-
-if ver == "3.8.10":
-	print("Correct Version Installed")
-else:
-	print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+for row in text.split("\n"):
+	send.append(row)         #making a list for each line of the commentary
 
 
-scorecard()
+ball=re.findall(r'([0-9][.][1-6]|[1-2][0-9][.][1-6])',text) #finding no of valid ball numbers
+
+
+while("" in send):
+    send.remove("") #removing empty characters in 'send' list
+
+
+for i in range(62):
+
+		send[i]=send[i][:3]+","+send[i][3:]
+
+for i in range(62,123):                      #adding extra comma after every ball number
+	
+		send[i]=send[i][:4]+","+send[i][4:]
 
 
 
+l=len(send)  # finding lenght of the list
 
+line=[]
 
+list1=[]
 
-#This shall be the last lines of the code.
-end_time = datetime.now()
-print('Duration of Program Execution: {}'.format(end_time - start_time))
+for i in range(l):
+	list1=send[i].split(',')
+	line.append(list1)        # splitting each line of commentary @ ','
+
+lx=len(line)
+batsman=[]
+bowlers=[]
+
+for i in range(lx):
+	players=line[i][1].split('to')	            
+	bowlers.append(players[0])     #appending all batsman and bowler names into respective lists
+	batsman.append(players[1])
+batsman=list(dict.fromkeys(batsman))   #removing duplicates
+bowlers=list(dict.fromkeys(bowlers))
+
+mybatsman=pd.DataFrame(0,batsman,['status','R','B','4s','6s','SR']) #creating a dataframe 'mybatsman' with required column and row names and initializing with 0's
+
+mybatsman['status']='not out' #initializing staus column with 'not out'
+
+mybowlers=pd.DataFrame(0,bowlers,['O','M','R','W','NB','WD','EC','B']) #creating a dataframe 'mybatsman' with required column and row names and initializing with 0's
+
+extra=0
+wide=0
+score=0
+wickets=0
+noball=0
+b=0
+lb=0
+fall=''
+
+content=[]
