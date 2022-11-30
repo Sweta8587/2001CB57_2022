4e8b06f8953586f9c7a8085f7466a8026f717be6
Sweta Kumari <74856144+Sweta8587@users.noreply.github.com>
2022-11-30 11:02:39 +0530

    pro 1
    p

--- proj1/proj1.py
+++ proj1/proj1.py
@@ -1,24 +1,61 @@
-
-from datetime import datetime
-start_time = datetime.now()
-
-#Help
-def proj_chat_tool():
-	pass
+#imports
+import socket 
+import threading
 
 
-###Code
+class ChatServer:
+    
+    clients_list = []
 
-from platform import python_version
-ver = python_version()
+    last_received_message = ""
 
-if ver == "3.8.10":
-	print("Correct Version Installed")
-else:
-	print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+    def __init__(self):
+        self.server_socket = None
+        self.create_listening_server()
+    #listen for incoming connection
+    def create_listening_server(self):
+    
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create a socket using TCP port and ipv4
+        local_ip = '127.0.0.1'
+        local_port = 10319
+        # this will allow you to immediately restart a TCP server
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # this makes the server listen to requests coming from other computers on the network
+        self.server_socket.bind((local_ip, local_port))
+        print("Listening for incoming messages..")
+        self.server_socket.listen(5) #listen for incomming connections / max 5 clients
+        self.receive_messages_in_a_new_thread()
+    #fun to receive new msgs
+    def receive_messages(self, so):
+        while True:
+            incoming_buffer = so.recv(256) #initialize the buffer
+            if not incoming_buffer:
+                break
+            self.last_received_message = incoming_buffer.decode('utf-8')
+            self.broadcast_to_all_clients(so)  # send to all clients
+        so.close()
+    #broadcast the message to all clients 
+    def broadcast_to_all_clients(self, senders_socket):
+        for client in self.clients_list:
+            socket, (ip, port) = client
+            if socket is not senders_socket:
+                socket.sendall(self.last_received_message.encode('utf-8'))
+
+    def receive_messages_in_a_new_thread(self):
+        while True:
+            client = so, (ip, port) = self.server_socket.accept()
+            self.add_to_clients_list(client)
+            print('Connected to ', ip, ':', str(port))
+            t = threading.Thread(target=self.receive_messages, args=(so,))
+            t.start()
+    #add a new client 
+    def add_to_clients_list(self, client):
+        if client not in self.clients_list:
+            self.clients_list.append(client)
 
 
-proj_chat_tool()
+if __name__ == "__main__":
+    ChatServer()
 
 
 
